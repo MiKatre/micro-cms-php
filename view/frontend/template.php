ffbdaa445901f571063367d939dc55fba12fc5a6
katre <0mica4420@gmail.com>
2018-07-02 15:18:30 +0200

    Add bootstrap locally and order scripts and styles

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -3,17 +3,15 @@
     <head>
         <meta charset="utf-8" />
         <title><?= $title ?></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-        
-
-        <link href="style.css" rel="stylesheet" /> 
+        <link href="static/thirdParty/bootstrap.min.css" rel="stylesheet" /> 
+        <link href="static/css/style.css" rel="stylesheet" /> 
     </head>
         
     <body>
         <?= $content ?>
 
+        <script src="static/thirdParty/bootstrap.bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <script src="static/js/main.js"></script>
     </body>
 </html>
