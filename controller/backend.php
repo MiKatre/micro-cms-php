f8354498185866489be817741586b89fd1aa464e
katre <0mica4420@gmail.com>
2018-07-09 13:27:33 +0200

    Login & Logout SESSION & COOKIE management

--- controller/backend.php
+++ controller/backend.php
@@ -6,36 +6,35 @@ require_once('model/User.php');
 
 
 function showLogin(){
+  session_start();
+  if (isset($_SESSION['name']) && !empty($_SESSION['name'])) {
+    header('Location:  index.php?action=showDashboard');
+  }
+
   require('view/backend/loginView.php');
 }
 
 function login($email, $password, $rememberMe = false) {
-
-  // IF session en course OR valid cookies
-
-  // Send directly to dashboard
-
-
   $userManager = new UserManager;
 
   $userData = $userManager->login($email);
 
   if($userData == false) {
     header('Location:  index.php?action=showLogin&errorMessage=Mauvais identifiant ou mot de passe !');
+    throw new Exception('Mauvais identifiant ou mot de passe');
   }
   
   $user = new User($userData);
   
   $isUser = password_verify($password, $user->hash());
+
   
   if(!$isUser) {
     header('Location:  index.php?action=showLogin&errorMessage=Mauvais identifiant ou mot de passe !');
+    throw new Exception('Mauvais identifiant ou mot de passe');
   }
 
-  throw new Exception($isUser . 'User object = ' . $user->hash());
 
-  // Else create user object
-  // register session info and redirect to the dashboard
   session_start();
   $_SESSION['id'] = $user->id();
   $_SESSION['name'] = $user->name();
@@ -43,11 +42,53 @@ function login($email, $password, $rememberMe = false) {
   $_SESSION['hash'] = $user->hash();
 
   if ($rememberMe) {
-    // Set Cookies
     setcookie('name', $user->name(), time() + 365*24*3600, null, null, false, true);
     setcookie('email', $user->email(), time() + 365*24*3600, null, null, false, true);
     setcookie('hash', $user->hash(), time() + 365*24*3600, null, null, false, true);
   }
 
   // Redirect to dashboard
+  header('Location:  index.php?action=showDashboard');
+}
+
+function logout() {
+  $_SESSION = array();
+  if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+  }
+  session_destroy();
+  header('Location:  index.php?');
+}
+
+function showDashboard() {
+ 
+  session_start();
+
+  if (!isset($_SESSION['name']) || empty($_SESSION['name'])) {
+    header('Location:  index.php?action=showLogin&errorMessage=Vous devez vous connecter pour avoir accès à cette page !');
+  }
+
+  // Ask model to fetch all Posts ordered by date and send back the objects.
+  $postManager = new PostManager();
+  $comments = $commentManager->getAllComments();
+
+  // Ask model to fetch all comments ordered by date and send back the objects.
+  $commentManager = new CommentManager();
+  $comments = $commentManager->getAllComments();
+
+  require('view/backend/dashboardView.php');
+}
+
+function updateComment($commentId, $status) {
+  $commentManager = new CommentManager();
+  $isSuccessful = $commentManager->status($commentId, $status);
+  if($isSuccessful) {
+    header('Location:  index.php?action=showDashboard&successMessage=Status du commentaire mis à jour !');
+  } else {
+    header('Location:  index.php?action=showDashboard&errorMessage=Impossible de mettre à jour le commentaire !');
+  }
 }
--- view/backend/template.php
+++ view/backend/template.php
@@ -12,7 +12,15 @@
     <?= $content ?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="static/thirdParty/jquery-3.3.1.min.js"><\/script>')</script>
     <script src="static/thirdParty/bootstrap.bundle.min.js"></script>
     <script src="static/js/main.js"></script>
+
+        <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+    
   </body>
 </html>
