47fc7aa1d22654447c467904858acf4acf40e4c8
katre <0mica4420@gmail.com>
2018-07-17 08:33:33 +0200

    Add footer

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -9,7 +9,7 @@
         
     <body>
 
-        <div id="" class="container-fluid">
+        <div class="container-fluid">
             <div class="row">
                 <div class="w-100">
                     <div class="header">
@@ -26,17 +26,13 @@
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav d-flex">
-                            <li class="nav-item ">
-                                <a class="nav-link" href="#">Blog</a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link" href="#">À Propos</a>
-                            </li>
-                            <li class="nav-item ml-auto">
-                                <a class="nav-link" target="_blank" rel="noopener" href="index.php?action=showLogin">Connexion</a>
-                            </li>
-
-                            </ul>
+                                <li class="nav-item ">
+                                    <a class="nav-link" href="#">Blog</a>
+                                </li>
+                                <li class="nav-item ">
+                                    <a class="nav-link" href="#">À Propos</a>
+                                </li>
+                            </ul>   
                         </div>
                     </nav>
 
@@ -44,6 +40,31 @@
                 </div>
             </div>
         </div>
+
+    <footer class="container-fluid py-5 bg-dark">
+      <div class="row main-size">
+        <div class="col-sm-4 text-white">
+            <a href="index.php?action=showLogin" target="_blank" class="text-light">
+            Jean Forteroche
+            </a>
+            <small class="d-block mb-3 text-muted">© 2017-2018</small>
+        </div>
+        <div class="col-sm-3">
+          <h5 class="text-light">Pages</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="index.php">Accueil</a></li>
+            <li><a class="text-muted" href="#">Blog</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-4">
+          <h5 class="text-light">À Propos</h5>
+          <ul class="list-unstyled text-small">
+            <li><a class="text-muted" href="#">À propos</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="static/thirdParty/bootstrap.bundle.min.js"></script>
         <script src="static/js/main.js"></script>
