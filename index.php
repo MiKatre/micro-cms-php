2fea87c89216401ad0ea36a89d2e5d2a799be897
katre <0mica4420@gmail.com>
2018-07-23 11:20:39 +0200

    replaced if else by switch statement

--- index.php
+++ index.php
@@ -3,31 +3,37 @@ require('controller/frontend.php');
 require('controller/backend.php');
 
 try {
-  if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'listPosts') {
-      listPosts();
-    }
-    elseif ($_GET['action'] == 'post') {
+  $action = (isset($_GET['action']) ? $_GET['action'] : 'justShowHome' );
+  switch ($action) {
+    case "listPosts":
+      $frontendController = new FrontendController; 
+      $frontendController->listPosts();
+      break;
+    case "post":
       if (isset($_GET['id']) && $_GET['id'] > 0) {
-        post();
+        $frontendController = new FrontendController; 
+        $frontendController->post();
       } else {
         throw new Exception('Missing id');
       }
-    }
-    elseif ($_GET['action'] == 'showBlog') {
+      break;
+    case "showBlog":
+      $frontendController = new FrontendController; 
       if(isset($_GET['page'])) {
-        showBlog($_GET['page']);
+        $frontendController->showBlog($_GET['page']);
       } else {
-        showBlog();
+        $frontendController->showBlog();
       }
-    }
-    elseif ($_GET['action'] == 'showAbout') {
-      showAbout();
-    }
-    elseif ($_GET['action'] == 'addComment') {
+      break;
+    case "showAbout":
+      $frontendController = new FrontendController; 
+      $frontendController->showAbout();
+      break;
+    case "addComment":
       if (isset($_GET['id']) && $_GET['id'] > 0) {
         if (!empty($_POST['author']) && !empty($_POST['content'])&& !empty($_POST['email'])) {
-          addComment($_GET['id'], $_POST['author'],$_POST['email'], $_POST['content'], $_POST['responseId']);
+          $frontendController = new FrontendController; 
+          $frontendController->addComment($_GET['id'], $_POST['author'],$_POST['email'], $_POST['content'], $_POST['responseId']);
         }
         else {
             throw new Exception('Tous les champs ne sont pas remplis !');
@@ -36,18 +42,19 @@ try {
       else {
         throw new Exception('Aucun identifiant de billet envoyé');
       }
-    }
-    elseif ($_GET['action'] == 'flagComment') {
+      break;
+    case "flagComment":
       if (isset($_GET['commentId']) && isset($_GET['postId']) && isset($_GET['status']) && $_GET['commentId'] > 0 &&  $_GET['postId'] > 0 ){
-        flagComment($_GET['commentId'], $_GET['postId'], $_GET['status'] );
+        $frontendController = new FrontendController; 
+        $frontendController->flagComment($_GET['commentId'], $_GET['postId'], $_GET['status'] );
       } else {
         throw new Exception('Impossible de signaler ce commentaire');
       }
-    }
-    elseif ($_GET['action'] == 'showLogin') {
+      break;
+    case "showLogin":
       showLogin();
-    }
-    elseif ($_GET['action'] == 'login') {
+      break;
+    case "login":
       if(isset($_POST['email']) && isset($_POST['password'])) {
         if(!empty($_POST['email']) && !empty($_POST['password'])) {
           $rememberMe = (isset($_POST['rememberMe']) ? true : false );
@@ -58,30 +65,30 @@ try {
       } else {
         throw new Exception('You should provide both an email and a passord');
       }
-    }
-    elseif ($_GET['action'] == 'showDashboard') {
+      break;
+    case "showDashboard":
       showDashboard();
-    }
-    elseif ($_GET['action'] == 'showDashboardPosts') {
+      break;
+    case "showDashboardPosts":
       if(isset($_GET['page'])) {
         showPaginatedPosts($_GET['page']);
       } else {
         showPaginatedPosts();
       }
-    }
-    elseif ($_GET['action'] == 'showDashboardComments') {
+      break;
+    case "showDashboardComments":
       if(isset($_GET['page'])) {
         showPaginatedComments($_GET['page']);
       } else {
         showPaginatedComments();
       }
-    }
-    elseif ($_GET['action'] == 'updateComment') {
+      break;
+    case "updateComment":
       if (isset($_GET['commentId']) && isset($_GET['status']) && isset($_GET['url'])) {
         updateComment($_GET['commentId'], $_GET['status'], $_GET['url']);
       }
-    }
-    elseif ($_GET['action'] == 'updatePost') {
+      break;
+    case "updatePost":
       if (isset($_GET['status']) && isset($_GET['postId']) &&  $_GET['postId'] > 0 ){
         updatePostStatus($_GET['postId'], $_GET['status'], $_GET['return'] );
       } elseif(isset($_GET['id']) && isset($_POST['title']) && isset($_POST['content']) && $_GET['id'] > 0) {
@@ -91,17 +98,18 @@ try {
       } else {
         throw new Exception('Impossible de publier cet article');
       }
-    }
-    elseif ($_GET['action'] == 'showEditor') {
+      break;
+    case "showEditor":
       showEditor($_GET['id']);
-    }
-    elseif ($_GET['action'] == 'logout') {
+      break;
+    case "logout":
       logout();
-    }
-  } else {
-    listPosts();
+      break;
+    default:
+      $frontendController = new FrontendController; 
+      $frontendController->listPosts();
+      break;
   }
-  
 } catch (Exception $e) {
   $errorMessage = $e->getMessage();
   require('view/errorView.php');
